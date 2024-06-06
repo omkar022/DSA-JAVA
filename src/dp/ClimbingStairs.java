1a88d7ad1e3da288c4a05b917a09a66283d34b2d
omkar022 <omkarjadhav0199@gmail.com>
2024-06-06 19:14:04 +0530

    dp

--- src/dp/ClimbingStairs.java
+++ src/dp/ClimbingStairs.java
@@ -2,17 +2,22 @@ package dp;
 
 public class ClimbingStairs {
     public static void main(String[] args) {
-        int n=6;
-        System.out.println(ways(n));
+        int n=60;
+        int memo[]=new int[n+1];
+        System.out.println(ways(n,memo));
     }
 
-    private static int ways(int n) {
+    private static int ways(int n,int memo[]) {
         if(n<0){
             return 0;
         }
         if (n==0){
             return 1;
         }
-        return ways(n-1)+ways(n-2);
+        if(memo[n]!=0){
+            return memo[n];
+        }
+        memo[n]= ways(n-1,memo)+ways(n-2,memo);
+        return memo[n];
     }
 }
