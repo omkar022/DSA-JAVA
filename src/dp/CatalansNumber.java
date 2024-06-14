7c200523bad6da1e18f0d9b386ed8889d119bac8
omkar022 <omkarjadhav0199@gmail.com>
2024-06-14 16:50:58 +0530

    dp Tab

--- src/dp/CatalansNumber.java
+++ src/dp/CatalansNumber.java
@@ -10,21 +10,14 @@ public class CatalansNumber {
     }
 
     private static int catalanTab(int n) {
-
-        int[] dp = new int[n + 1];
-        dp[0] = 1;
-        dp[1] = 1;
-
-
-        for (int i = 2; i < n + 1; i++) {
-            int ans = 0;
-
-            for (int j = 0; j < i; j++) {
-                ans += dp[j] * dp[i - j - 1];
+        int dp[]=new int[n+1];
+        dp[0]=1;
+        dp[1]=1;
+        for(int i=2;i<n+1;i++){
+            for(int j=0;j<i;j++){
+                dp[i]+=dp[j]*dp[i-j-1];
             }
-            dp[i] = ans;
         }
-
         return dp[n];
     }
 
