b1aa0aceb0590776b1e45139ffa3483bf7596799
omkar022 <omkarjadhav0199@gmail.com>
2024-06-16 18:14:27 +0530

    st

--- src/st/SegmentTrees.java
+++ src/st/SegmentTrees.java
@@ -57,6 +57,7 @@ public class SegmentTrees {
         return tree[i];
     }
 
+
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5, 6, 7, 8};
         int n = arr.length;
