aa85081ee8a7906dea5330e11ac4078ff8cbe484
omkar022 <omkarjadhav0199@gmail.com>
2024-07-06 16:16:51 +0530

    PQ

--- src/heap/MinHeap.java
+++ src/heap/MinHeap.java
@@ -1,19 +1,20 @@
 package heap;
 
 import java.util.ArrayList;
+
 // 3 place sign chnages
 public class MinHeap {
     public static void main(String[] args) {
-          Heap h=new Heap();
-          h.add(3);
-          h.add(4);
-          h.add(1);
-          h.add(5);
+        Heap h = new Heap();
+        h.add(3);
+        h.add(4);
+        h.add(1);
+        h.add(5);
 
-          while (!h.isEmpty()){
-              System.out.println(h.peek());
-              h.remove();
-          }
+        while (!h.isEmpty()) {
+            System.out.println(h.peek());
+            h.remove();
+        }
 
     }
 
@@ -29,8 +30,8 @@ public class MinHeap {
                 int temp = arr.get(x);
                 arr.set(x, arr.get(par));
                 arr.set(par, temp);
-                x=par;
-                par=(x - 1) / 2;
+                x = par;
+                par = (x - 1) / 2;
             }
 
         }
@@ -59,23 +60,23 @@ public class MinHeap {
             int left = 2 * i + 1;
             int right = 2 * i + 2;
             int minIdx = i;
-            if (left < arr.size() && arr.get(minIdx) > arr.get(left)) {
+            if (left < arr.size() &&  arr.get(left) < arr.get(minIdx) ) {
                 minIdx = left;
             }
-            if (right < arr.size() && arr.get(minIdx) > arr.get(right)) {
+            if (right < arr.size() && arr.get(right)< arr.get(minIdx) ) {
                 minIdx = right;
             }
             if (minIdx != i) {
-                int temp= arr.get(i);
-                arr.set(i,arr.get(minIdx));
-                arr.set(minIdx,temp);
+                int temp = arr.get(i);
+                arr.set(i, arr.get(minIdx));
+                arr.set(minIdx, temp);
                 heapify(minIdx);
 
             }
         }
 
-        public boolean isEmpty(){
-            return arr.size()==0;
+        public boolean isEmpty() {
+            return arr.size() == 0;
         }
 
     }
