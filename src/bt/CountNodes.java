14e5ef67245e612a8f5b1ea94a608d1f5fc344ed
omkar022 <omkarjadhav0199@gmail.com>
2024-08-09 16:47:29 +0530

    BT

---
+++ src/bt/CountNodes.java
@@ -0,0 +1,36 @@
+package bt;
+
+public class CountNodes {
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+        System.out.println(count(root));
+    }
+
+    private static int count(Node root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = count(root.left);
+        int right = count(root.right);
+        return left + right + 1;
+    }
+
+
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+}
