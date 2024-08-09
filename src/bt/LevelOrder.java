9d167e8dd3fe9729a5fff886df88617e90976b5b
omkar022 <omkarjadhav0199@gmail.com>
2024-08-09 16:29:08 +0530

    BT

---
+++ src/bt/LevelOrder.java
@@ -0,0 +1,66 @@
+package bt;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class LevelOrder {
+    static int i = -1;
+
+    public static Node buildTree(int[] nodes) {
+        i++;
+        if (nodes[i] == -1) {
+            return null;
+        }
+        Node root = new Node(nodes[i]);
+        root.left = buildTree(nodes);
+        root.right = buildTree(nodes);
+        return root;
+    }
+
+    public static void levelorder(Node root) {
+        if (root == null) {
+            return;
+        }
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+        q.add(null);
+        while (!q.isEmpty()) {
+            Node curr = q.remove();
+            if (curr == null) {
+                System.out.println();
+                if (q.isEmpty()) {
+                    break;
+                } else {
+                    q.add(null);
+                }
+            } else {
+                System.out.print(curr.data + " ");
+                if (curr.left != null) {
+                    q.add(curr.left);
+                }
+                if (curr.right != null) {
+                    q.add(curr.right);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nodes = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
+        Node root = buildTree(nodes);
+        levelorder(root);
+    }
+
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+
+    }
+}
