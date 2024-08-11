28fc100e6e592cce15f15755932c689993c7a45f
omkar022 <omkarjadhav0199@gmail.com>
2024-08-11 10:05:54 +0530

    BT

---
+++ src/bt/TranformToSumTree.java
@@ -0,0 +1,48 @@
+package bt;
+
+public class TranformToSumTree {
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+        System.out.println(transform(root));
+        System.out.println();
+        preorder(root);
+
+    }
+
+    public static void preorder(Node root) {
+        if (root == null) {
+            return;
+        }
+        System.out.print(root.data + " ");
+        preorder(root.left);
+        preorder(root.right);
+    }
+
+    public static int transform(Node root) {
+        if (root == null) {
+            return 0;
+        }
+        int sum = transform(root.left) + transform(root.right);
+        int data = root.data;
+        root.data = sum;
+        return sum + data;
+    }
+
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+}
