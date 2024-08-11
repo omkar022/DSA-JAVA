c9150f6bccf5cad3bbe44ecd809f330d2f6973a2
omkar022 <omkarjadhav0199@gmail.com>
2024-08-11 13:39:44 +0530

    BT

---
+++ src/bt/UnivalTree.java
@@ -0,0 +1,37 @@
+package bt;
+
+public class UnivalTree {
+    public static void main(String[] args) {
+        Node root = new Node(2);
+        root.left = new Node(2);
+        root.right = new Node(2);
+        root.left.left = new Node(2);
+        root.left.right = new Node(2);
+        System.out.println(isUnivalTree(root));
+    }
+
+    private static boolean isUnivalTree(Node root) {
+        if (root == null) {
+            return true;
+        }
+        if (root.left != null && root.left.data != root.data) {
+            return false;
+        }
+        if (root.right != null && root.right.data != root.data) {
+            return false;
+        }
+        return isUnivalTree(root.left) && isUnivalTree(root.right);
+    }
+
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+}
