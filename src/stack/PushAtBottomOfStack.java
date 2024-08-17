fbfa1073a4fcc605695aac3f0309fbc9c275c4b2
omkar022 <omkarjadhav0199@gmail.com>
2024-08-17 14:05:53 +0530

    Stack

---
+++ src/stack/PushAtBottomOfStack.java
@@ -0,0 +1,27 @@
+package stack;
+
+import java.util.Stack;
+
+public class PushAtBottomOfStack {
+    public static void main(String[] args) {
+        Stack<Integer>s=new Stack<>();
+        s.add(1);
+        s.add(2);
+        s.add(3);
+        System.out.println(s);
+        pushAtBottom(s,4);
+        System.out.println(s);
+    }
+
+    private static void pushAtBottom(Stack<Integer> s,int data) {
+        Stack<Integer>ns=new Stack<>();
+        while (!s.isEmpty()){
+            ns.push(s.pop());
+        }
+        ns.add(data);
+
+        while (!ns.isEmpty()){
+            s.push(ns.pop());
+        }
+    }
+}
