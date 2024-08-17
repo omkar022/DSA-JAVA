ae1268c95b6e96dc8b5d00e143b415c772f64781
omkar022 <omkarjadhav0199@gmail.com>
2024-08-17 13:38:39 +0530

    Stack

---
+++ src/stack/StackCodeLL.java
@@ -0,0 +1,60 @@
+package stack;
+
+public class StackCodeLL {
+
+    public static void main(String[] args) {
+
+        Stack s = new Stack();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        while (!s.isEmpty()) {
+            System.out.println(s.peek());
+            s.pop();
+        }
+
+    }
+
+    static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    static class Stack {
+        Node head;
+
+
+        public boolean isEmpty() {
+            return head == null;
+        }
+
+        public void push(int data) {
+            Node newNode = new Node(data);
+            /**/
+            newNode.next = head;
+            head = newNode;
+        }
+
+        public int pop() {
+            if (isEmpty()) {
+                return -1;
+            }
+            int top = head.data;
+            head = head.next;
+            return top;
+        }
+
+        public int peek() {
+            if (isEmpty()) {
+                return -1;
+            }
+            return head.data;
+        }
+
+    }
+}
