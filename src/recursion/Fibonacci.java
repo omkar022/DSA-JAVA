20a1ab7673627e5c5e823f8f2ed0dfe6554043c2
omkar022 <omkarjadhav0199@gmail.com>
2024-09-30 18:31:54 +0530

    recursion

---
+++ src/recursion/Fibonacci.java
@@ -0,0 +1,14 @@
+package recursion;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        System.out.println(fibo(6));
+    }
+
+    private static int fibo(int n) {
+        if (n == 0 || n == 1) {
+            return n;
+        }
+        return fibo(n - 1) + fibo(n - 2);
+    }
+}
