92d3b3106dd2bce00c55781304147e6aaff66ea2
omkar022 <omkarjadhav0199@gmail.com>
2024-10-05 18:04:42 +0530

    string

---
+++ src/strings/Solution.java
@@ -0,0 +1,26 @@
+package strings;
+
+class Solution {
+    public static String compressedString(String word) {
+        int n = word.length();
+        StringBuilder sb = new StringBuilder();
+        int i = 0;
+        while (i < n) {
+            char currentChar = word.charAt(i);
+            int count = 0;
+            while (i < n && word.charAt(i) == currentChar && count < 9) {
+                count++;
+                i++;
+            }
+            sb.append(count).append(currentChar);
+        }
+
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        String s = "aaaaaaaaaaaaaabb";
+        System.out.println(compressedString(s));
+
+    }
+}
